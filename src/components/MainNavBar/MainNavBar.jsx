afca8183a331c5908fb2c1354050fb981027ce06
HaysS <stanfordhays@gmail.com>
2018-10-12 20:40:07 -0500

    changed nav bar link

--- src/components/MainNavBar/MainNavBar.jsx
+++ src/components/MainNavBar/MainNavBar.jsx
@@ -13,13 +13,10 @@ class MainNavBar extends Component {
           <Link to='/'>
             <h3 style={{flex: '1', color: '#f7f7f7'}}>Hays Stanford</h3>
           </Link>
-          <div style={{flex: '1', color: '#f7f7f7'}}></div>
+          <div style={{flex: '2', color: '#f7f7f7'}}></div>
           <div style={{flex: '2', display: 'flex', margin: 'auto'}}>
             <Link to='/blog/' style={{flex: '1'}}>
-              <h5 style={{color: '#f7f7f7'}}>Read</h5>
-            </Link>
-            <Link to='/free/' style={{flex: '1'}}>
-              <h5 style={{color: '#f7f7f7'}}>Free Stuff</h5>
+              <h5 style={{color: '#f7f7f7'}}>Blog</h5>
             </Link>
             <Link to='/about/' style={{flex: '1'}}>
               <h5 style={{color: '#f7f7f7'}}>About</h5>
